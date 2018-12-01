b76e5de9438cfe48f1c984ee8255b54de885ff5d
Biplov Kumar <biplovkumar.kumar@gmail.com>
2018-12-01 16:19:43 +0530

    init

---
+++ colorlibrary/src/main/java/com/codeinmood/silentgamer/colorlibrary/Biplov.java
@@ -0,0 +1,12 @@
+package com.codeinmood.silentgamer.colorlibrary;
+
+public class Biplov {
+
+    public Biplov() {
+    }
+
+    public String getCLassName(){
+
+        return getCLassName();
+    }
+}
